24e94ced304b58febf3e3ccf31cd0a2e904d2e83
Sebastian Fudalej <sebastianfudalej@gmail.com>
2022-04-05 00:04:40 +0200

    feat: hero page with logo and text

--- packages/frontend/src/components/Hero/Hero.tsx
+++ packages/frontend/src/components/Hero/Hero.tsx
@@ -3,14 +3,20 @@ import "./Hero.scss";
 function Hero() {
   return (
     <div id="hero" className="fullscreen Hero">
+      <div className="left-wave">
+        <img src="bgb.svg" alt="University Championships" />
+      </div>
+      <div className="right-wave">
+        <img src="bgu.svg" alt="University Championships" />
+      </div>
+      <section className="mobileLogo">
+        <img src="logo.svg" alt="University Championships" />
+      </section>
       <section className="logo">
-        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-          <path stroke-width="4" fill="#FF0066" d="M72.9,-21.4C82.6,6.1,70.3,42.9,48.3,56.6C26.2,70.3,-5.5,60.8,-28.8,43.1C-52.2,25.3,-67,-0.6,-60.8,-23.3C-54.6,-46,-27.3,-65.4,2.2,-66.1C31.6,-66.8,63.3,-48.8,72.9,-21.4Z" transform="translate(100 100)" />
-        </svg>
-
+        <img src="puchar.svg" alt="University Championships" />
       </section>
       <section className="uc-text" style={{ userSelect: "none" }}>
-        <img src="university_championships.svg" alt="University Championships" />
+        <img src="text.svg" alt="University Championships" />
       </section>
     </div>
   );
